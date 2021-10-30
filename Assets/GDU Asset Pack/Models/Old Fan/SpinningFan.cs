0ee6d45483c7f3ad7179ffe98aac3a7acc9bc435
Sergio Oliva <sergio.oliva@gmail.com>
2021-10-30 17:22:26 +0200

    Initial commit

---
+++ Assets/GDU Asset Pack/Models/Old Fan/SpinningFan.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningFan : MonoBehaviour
+{
+    // Just drag and drop this script onto the blades mesh!
+
+    public float SpinningSpeed = 700;
+
+    void Update()
+    {
+        transform.Rotate(SpinningSpeed * Time.deltaTime, 0, 0); //rotates 50 degrees per second around x axis
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# IntrepidLou
+ IntrepidLou
